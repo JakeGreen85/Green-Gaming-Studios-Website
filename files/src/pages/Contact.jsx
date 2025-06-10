ff993f3ea4e3c5d156b7c0269fe02ce946b1d2a2
JakeGreen85 <jakg@itu.dk>
2025-06-11 01:12:44 +0200

    Initial commit

---
+++ files/src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+function Contact() {
+    return (
+      <div>
+        <h1>Welcome to Smith Services</h1>
+        <p>We provide top-notch solutions for local businesses.</p>
+      </div>
+    );
+  }
+  
+export default Contact;
---
+++ files/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+      <div>
+        <h1>Welcome to Smith Services</h1>
+        <p>We provide top-notch solutions for local businesses.</p>
+      </div>
+    );
+  }
+  
+export default Home;
