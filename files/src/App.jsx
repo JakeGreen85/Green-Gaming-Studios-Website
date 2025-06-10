35f88c257b27bd744577f6523cee7019bd853f50
JakeGreen85 <jakg@itu.dk>
2025-06-11 01:29:38 +0200

    Added base URL

--- files/src/App.jsx
+++ files/src/App.jsx
@@ -6,7 +6,7 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/services" element={<Services />} />
